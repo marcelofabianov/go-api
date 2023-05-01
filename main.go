ba6c68c8d46e32c20a0f25f3a46227de284b0aa5
Marcelo Fabiano <marcelofabianov@gmail.com>
2023-05-01 14:18:12 -0300

    chore: add servic nginx and postgres docker-compose.yml

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type Response struct {
+	Data   []interface{} `json:"data"`
+	Status Status        `json:"status"`
+}
+
+type Status struct {
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+	Success bool   `json:"success"`
+}
+
+var response = Response{
+	Data: []interface{}{},
+	Status: Status{
+		Code:    200,
+		Message: "OK",
+		Success: true,
+	},
+}
 
 func main() {
-    fmt.Println("Hello World!")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		json.NewEncoder(w).Encode(response)
+	})
+
+	http.ListenAndServe(":8080", nil)
 }
